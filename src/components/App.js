53417d645dc1d72e8e92e44dc17025a63706f7d4
Damian Szalbierz <szalbierz.d.k@gmail.com>
2018-12-18 00:35:44 +0100

    Yields do not resolve

--- src/components/App.js
+++ src/components/App.js
@@ -2,22 +2,27 @@ import React, { Component } from 'react';
 import ChartsGroup from './ChartsGroup';
 import Map from './Map';
 import MeteoDataLoader from '../utils/MeteoDataLoader';
+import YieldsDataLoader from '../utils/YieldsDataLoader';
 import './App.css';
 
 class App extends Component {
-    state ={
-        loading: true
+    state = {
+        meteo: false,
+        yields: false
     }
 
     componentDidMount() {
-        MeteoDataLoader.loadData().then(() => this.setState({loading: false}))
+        MeteoDataLoader.loadData().then(() => this.setState({meteo: true}))
+        YieldsDataLoader.loadData().then(() => this.setState({yields: true})); // Nie chce się zrobić resolve!!!
     }
 
     render() {
         return (
             <div className="App">
                 <Map />
-                {!this.state.loading && <ChartsGroup regions={['PL-DS', 'PL-WP', 'PL-KP', 'PL-PM']}/>}
+                {this.state.loading && 
+                    this.state.yields &&
+                    <ChartsGroup regions={['PL-DS', 'PL-WP', 'PL-KP', 'PL-PM']}/>}
             </div>
         );
     }
