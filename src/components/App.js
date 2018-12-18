1ffcc22c66d7755b7b5e8ef0ac0f2c93aef9923c
Damian Szalbierz <szalbierz.d.k@gmail.com>
2018-12-18 16:32:36 +0100

    Map with fusionCharts

--- src/components/App.js
+++ src/components/App.js
@@ -2,26 +2,21 @@ import React, { Component } from 'react';
 import ComboChart from './charts/ComboChart';
 import Map from './Map';
 import MeteoDataLoader from '../utils/MeteoDataLoader';
-import YieldsDataLoader from '../utils/YieldsDataLoader.1';
 import './App.css';
 
 class App extends Component {
-    render() {
-        MeteoDataLoader.loadData().then(() => {
-            new MeteoDataLoader().avgOf()
-            new MeteoDataLoader().avgOf('temp', 2003, 'PL-WP', 5, 8)
-            new MeteoDataLoader().avgOf('temp', 2004, 'PL-WP', 5, 8)
-            new MeteoDataLoader().avgOf('temp', 2007, 'PL-WP', 5, 8)
-            new MeteoDataLoader().avgOf('rainfall', 2016, 'PL-DS', 5, 8)
-            new MeteoDataLoader().avgOf('humidity', 2011, 'PL-DS', 5, 8)
-            console.log(new MeteoDataLoader().avgOf('rainfall', 2013, 'PL-DS', 5, 8))
-        })
+    state = {
+        meteoLoaded: false
+    }
 
-        
+    componentDidMount() {
+        MeteoDataLoader.loadData().then(() => this.setState({ meteoLoaded: true }))
+    }
+
+    render() {
         return (
             <div className="App">
                 <Map />
-                <button onClick={() => new MeteoDataLoader().avgOf()}>check avg</button>
                 <ComboChart
                     title={"Wielkość plonów w stosunku do opadów"}
                     range={{start: "2003", stop: "2016"}}
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,34 +1,103 @@
-import React from "react";
-import { Chart } from 'react-google-charts'
+// Step 1 - Including react
+import React from 'react';
 
-export default class Map extends React.Component {
-    render() {
-        return (
-            <div style={{display: 'block', maxWidth: "100%"}}>
-                <Chart
-                    chartType="GeoChart"
-                    data={[
-                        ['Województwo', 'Wielkość plonów',"Opady"],
-                        ['PL-DS', 200, 100],
-                        ['PL-KP', 300, 130],
-                        ['PL-LU', 400, 150],
-                        ['PL-LB', 500, 120],
-                    ]}
-                    options={{
-                        colorAxis: { colors: ['#00853f', 'yellow', '#e31b23'] },
-                        chartArea: { width: '30%' },
-                        region: "PL",
-                        displayMode: 'regions',
-                        resolution: 'provinces',
-                        series:{
-                            1: {
-                                displayMode: "markers",
-                                targetAxisIndex:1
-                            }
-                        }
-                    }}
-                />
-            </div>
-        )
+// Step 2 - Including the react-fusioncharts component
+import ReactFC from 'react-fusioncharts';
+
+// Step 3 - Including the fusioncharts library
+import FusionCharts from 'fusioncharts';
+
+// Step 4 - Including the map renderer
+import FusionMaps from 'fusioncharts/fusioncharts.maps';
+
+// Step 5 - Including the map definition file
+// import World from 'fusioncharts/maps/fusioncharts.world';
+import Poland from 'fusionmaps/maps/fusioncharts.poland';
+
+// Step 6 - Including the theme as fusion
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+
+// Step 7 - Adding the map as dependency to the core fusioncharts
+ReactFC.fcRoot(FusionCharts, FusionMaps, Poland, FusionTheme);
+
+// Step 8 - Creating the JSON object to store the map configurations
+const chartConfigs = {
+    type: 'maps/poland',
+    width: '800',
+    height: '550',
+    dataFormat: 'json',
+    dataSource: {
+    // Map Configuration
+        "chart": {
+                "caption": "Average Yield in Poland",
+                "subcaption": " 2003-2016",
+                "numbersuffix": "%",
+                "includevalueinlabels": "1",
+                "labelsepchar": ": ",
+                "entityFillHoverColor": "#FFF9C4",
+                "theme": "fusion"
+        },
+        // Aesthetics; ranges synced with the slider
+        "colorrange": {
+            "minvalue": "0",
+            "code": "#FFE0B2",
+            "gradient": "1",
+            "color": [{
+                "minvalue": "0.5",
+                "maxvalue": "1.0",
+                "color": "#FFD74D"
+            }, {
+                "minvalue": "1.0",
+                "maxvalue": "2.0",
+                "color": "#FB8C00"
+            }, {
+                "minvalue": "2.0",
+                "maxvalue": "3.0",
+                "color": "#E65100"
+            }]
+        },
+        // Source data as JSON --> id represents countries of world.
+        "data": [{
+            "id": "NA",
+            "value": ".82",
+            "showLabel": "1"
+        }, {
+            "id": "SA",
+            "value": "2.04",
+            "showLabel": "1"
+        }, {
+            "id": "AS",
+            "value": "1.78",
+            "showLabel": "1"
+        }, {
+            "id": "EU",
+            "value": ".40",
+            "showLabel": "1"
+        }, {
+            "id": "AF",
+            "value": "2.58",
+            "showLabel": "1"
+        }, {
+            "id": "AU",
+            "value": "1.30",
+            "showLabel": "1"
+        }]
     }
 }
+// Step 8 - Creating the DOM element to pass the react-fusioncharts component 
+class Map extends React.Component {
+  render() {
+     return (
+     <ReactFC
+        {...chartConfigs}
+        events={{
+            entityClick: function (event, args) {
+                console.log(args.label + 'clicked');
+            }}
+        }
+        />
+     );
+  }
+}
+
+export default Map
