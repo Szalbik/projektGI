ac31f6d63a87be06cd64ca6f23188aed441ac927
Damian Szalbierz <szalbierz.d.k@gmail.com>
2018-12-18 21:31:56 +0100

    Change color on click

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
 import ComboChart from './charts/ComboChart';
 import Map from './Map';
 import MeteoDataLoader from '../utils/MeteoDataLoader';
@@ -6,17 +7,97 @@ import './App.css';
 
 class App extends Component {
     state = {
-        meteoLoaded: false
+        meteoLoaded: false,
+        configCharts: {
+            type: 'maps/poland',
+            width: '800',
+            height: '550',
+            dataFormat: 'json',
+            dataSource: {
+            // Map Configuration
+                "chart": {
+                        "caption": "Average Yield in Poland",
+                        "subcaption": " 2003-2016",
+                        "numbersuffix": "",
+                        "includevalueinlabels": "1",
+                        "labelsepchar": ": ",
+                        "entityFillHoverColor": "#FFF9C4",
+                        "theme": "fusion"
+                },
+                // Aesthetics; ranges synced with the slider
+                "colorrange": {
+                    "minvalue": "0",
+                    "code": "#FFE0B2",
+                    "gradient": "1",
+                    "color": [{
+                        "minvalue": "0.5",
+                        "maxvalue": "300",
+                        "color": "#FFD74D"
+                    }, {
+                        "minvalue": "300",
+                        "maxvalue": "700",
+                        "color": "#FB8C00"
+                    }, {
+                        "minvalue": "700",
+                        "maxvalue": "1000",
+                        "color": "#E65100"
+                    }]
+                },
+                // Source data as JSON --> id represents regions of Poland.
+                "data": [
+                    { "id": "74", "value": Math.random()*1000, "label": "Łódzkie", "shortLabel": "LD" }, 
+                    { "id": "72", "value": Math.random()*1000, "label": "Dolnośląskie", "shortLabel": "DS" }, 
+                    { "id": "73", "value": Math.random()*1000, "label": "Kujawsko-Pomorskie", "shortLabel": "KP" }, 
+                    { "id": "75", "value": Math.random()*1000, "label": "Lubelskie", "shortLabel": "LU" }, 
+                    { "id": "76", "value": Math.random()*1000, "label": "Lubuskie", "shortLabel": "LB" }, 
+                    { "id": "78", "value": Math.random()*1000, "label": "Mazowieckie", "shortLabel": "MZ" }, 
+                    { "id": "77", "value": Math.random()*1000, "label": "Małopolskie", "shortLabel": "MA" }, 
+                    { "id": "79", "value": Math.random()*1000, "label": "Opolskie", "shortLabel": "OP" }, 
+                    { "id": "80", "value": Math.random()*1000, "label": "Podkarpackie", "shortLabel": "PK" }, 
+                    { "id": "81", "value": Math.random()*1000, "label": "Podlaskie", "shortLabel": "PD" }, 
+                    { "id": "82", "value": Math.random()*1000, "label": "Pomorskie", "shortLabel": "PM" }, 
+                    { "id": "85", "value": Math.random()*1000, "label": "Warmińsko-Mazurskie", "shortLabel": "WN" }, 
+                    { "id": "86", "value": Math.random()*1000, "label": "Wielkopolskie", "shortLabel": "WP" }, 
+                    { "id": "87", "value": Math.random()*1000, "label": "Zachodniopomorskie", "shortLabel": "ZP", color: 'black' }, 
+                    { "id": "83", "value": Math.random()*1000, "label": "Śląskie", "shortLabel": "SL" }, 
+                    { "id": "84", "value": Math.random()*1000, "label": "Świętokrzyskie", "shortLabel": "SK" }, 
+                ]
+            }
+        },
+        regions: []
     }
 
     componentDidMount() {
         MeteoDataLoader.loadData().then(() => this.setState({ meteoLoaded: true }))
     }
 
+    toggleRegion = (event, args) => {
+        const { regions } = this.state;
+        // console.log(this.state.configCharts.dataSource);
+        const { configCharts } = this.state;
+        const { dataSource } = this.state.configCharts;
+        const { data } = this.state.configCharts.dataSource;
+        const newData = data.filter(elem => elem.shortLabel !== args.shortLabel)
+        let row = data.find(elem => elem.shortLabel === args.shortLabel)
+        if (regions.includes(args.shortLabel)) {
+            delete row['color']
+            this.setState({ configCharts: { ...configCharts, dataSource: { ...dataSource, data: [...newData, { ...row } ] }  } })
+        } else {
+            this.setState({ configCharts: { ...configCharts, dataSource: { ...dataSource, data: [...newData, { ...row, color: "black" } ] }  } })
+        }
+        
+        if (regions.includes(args.shortLabel)) {
+            let newRegions = regions.filter(region => region !== args.shortLabel)
+            this.setState({regions: newRegions})
+        } else {
+            this.setState({regions: [...this.state.regions, args.shortLabel]})
+        }
+    }
+
     render() {
         return (
             <div className="App">
-                <Map />
+                <Map chartConfigs={this.state.configCharts} toggleRegion={this.toggleRegion} />
                 <ComboChart
                     title={"Wielkość plonów w stosunku do opadów"}
                     range={{start: "2003", stop: "2016"}}
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,103 +1,18 @@
-// Step 1 - Including react
 import React from 'react';
-
-// Step 2 - Including the react-fusioncharts component
 import ReactFC from 'react-fusioncharts';
-
-// Step 3 - Including the fusioncharts library
 import FusionCharts from 'fusioncharts';
-
-// Step 4 - Including the map renderer
 import FusionMaps from 'fusioncharts/fusioncharts.maps';
-
-// Step 5 - Including the map definition file
-// import World from 'fusioncharts/maps/fusioncharts.world';
 import Poland from 'fusionmaps/maps/fusioncharts.poland';
-
-// Step 6 - Including the theme as fusion
 import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
 
-// Step 7 - Adding the map as dependency to the core fusioncharts
 ReactFC.fcRoot(FusionCharts, FusionMaps, Poland, FusionTheme);
 
-// Step 8 - Creating the JSON object to store the map configurations
-const chartConfigs = {
-    type: 'maps/poland',
-    width: '800',
-    height: '550',
-    dataFormat: 'json',
-    dataSource: {
-    // Map Configuration
-        "chart": {
-                "caption": "Average Yield in Poland",
-                "subcaption": " 2003-2016",
-                "numbersuffix": "",
-                "includevalueinlabels": "1",
-                "labelsepchar": ": ",
-                "entityFillHoverColor": "#FFF9C4",
-                "theme": "fusion"
-        },
-        // Aesthetics; ranges synced with the slider
-        "colorrange": {
-            "minvalue": "0",
-            "code": "#FFE0B2",
-            "gradient": "1",
-            "color": [{
-                "minvalue": "0.5",
-                "maxvalue": "300",
-                "color": "#FFD74D"
-            }, {
-                "minvalue": "300",
-                "maxvalue": "700",
-                "color": "#FB8C00"
-            }, {
-                "minvalue": "700",
-                "maxvalue": "1000",
-                "color": "#E65100"
-            }]
-        },
-        // Source data as JSON --> id represents countries of world.
-        "data": [
-            { "id": "74", "value": Math.random()*1000, "label": "Łódzkie", "shortLabel": "LD" }, 
-            { "id": "72", "value": Math.random()*1000, "label": "Łódzkie", "shortLabel": "DS" }, 
-            { "id": "73", "value": Math.random()*1000, "label": "Łódzkie", "shortLabel": "KP" }, 
-            { "id": "75", "value": Math.random()*1000, "label": "Łódzkie", "shortLabel": "LU" }, 
-            { "id": "76", "value": Math.random()*1000, "label": "Łódzkie", "shortLabel": "LB" }, 
-            { "id": "77", "value": Math.random()*1000, "label": "Łódzkie", "shortLabel": "MZ" }, 
-            { "id": "78", "value": Math.random()*1000, "label": "Łódzkie", "shortLabel": "MA" }, 
-            { "id": "79", "value": Math.random()*1000, "label": "Łódzkie", "shortLabel": "OP" }, 
-            { "id": "80", "value": Math.random()*1000, "label": "Łódzkie", "shortLabel": "PK" }, 
-            { "id": "81", "value": Math.random()*1000, "label": "Łódzkie", "shortLabel": "PD" }, 
-            { "id": "82", "value": Math.random()*1000, "label": "Łódzkie", "shortLabel": "PM" }, 
-            { "id": "83", "value": Math.random()*1000, "label": "Łódzkie", "shortLabel": "WN" }, 
-            { "id": "84", "value": Math.random()*1000, "label": "Łódzkie", "shortLabel": "WP" }, 
-            { "id": "85", "value": Math.random()*1000, "label": "Łódzkie", "shortLabel": "ZP" }, 
-            { "id": "86", "value": Math.random()*1000, "label": "Łódzkie", "shortLabel": "SL" }, 
-            { "id": "87", "value": Math.random()*1000, "label": "Łódzkie", "shortLabel": "SK" }, 
-        ]
-    }
-}
-// Step 8 - Creating the DOM element to pass the react-fusioncharts component 
 class Map extends React.Component {
-    state = {
-        regions: []
-    }
-
-    toggleRegion = (event, args) => {
-        const { regions } = this.state;
-        if (regions.includes(args.shortLabel)) {
-            let newRegions = regions.filter(region => region !== args.shortLabel)
-            this.setState({regions: newRegions})
-        } else {
-            this.setState({regions: [...this.state.regions, args.shortLabel]})
-        }
-    }
-
   render() {
      return (
      <ReactFC
-        {...chartConfigs}
-        fcEvent-entityClick={this.toggleRegion}
+        {...this.props.chartConfigs}
+        fcEvent-entityClick={this.props.toggleRegion}
         />
      );
   }
