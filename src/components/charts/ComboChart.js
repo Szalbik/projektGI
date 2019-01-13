e4bfdc1fe4580a37b742541bcf421f5c3616a89e
wbelka94 <wbelka94@gmail.com>
2019-01-13 21:08:08 +0100

    fix - stop display zeros for meto data

--- src/components/charts/ComboChart.js
+++ src/components/charts/ComboChart.js
@@ -38,6 +38,7 @@ export default class ComboChart extends Component {
                 let p;
                 if (s.type === 'meteo') {
                     p = new MeteoDataLoader().avgOf(s.value, +year, s.region, 3, 9);
+                    p = p === 0 ? null : p;
                 }
                 else if (s.type === 'yield') {
                     p = YieldsDataLoader.single(year, s.value, 'dt/ha', s.region);
