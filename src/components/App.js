0a1e9b8a33685941bf15ecd8afc1a23eb72768ff
Damian Szalbierz <szalbierz.d.k@gmail.com>
2019-01-13 18:27:29 +0100

    Update MeteoDataLoader to return avg for all years

--- src/components/App.js
+++ src/components/App.js
@@ -1,124 +1,129 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Map from "./Map";
 import ChartsGroup from "./ChartsGroup";
 import MeteoDataLoader from "../utils/MeteoDataLoader";
 import YieldsDataLoader from "../utils/YieldsDataLoader";
 import Regions from "../utils/Regions";
-import FusionChartConfig from '../utils/FusionChartConfig';
+import FusionChartConfig from "../utils/FusionChartConfig";
 import "./App.css";
 
 class App extends Component {
-    state = {
-        meteoLoaded: false,
-        yieldsLoaded: false,
-        regionsDataLoaded: false,
-        configCharts: FusionChartConfig,
-        regions: ["PL-WP", "PL-DS"]
-    };
+  state = {
+    meteoLoaded: false,
+    yieldsLoaded: false,
+    regionsDataLoaded: false,
+    configCharts: FusionChartConfig,
+    regions: ["PL-WP", "PL-DS"]
+  };
 
-    componentDidMount() {
-        const {configCharts} = this.state;
-        const {dataSource} = this.state.configCharts;
+  componentDidMount() {
+    const { configCharts } = this.state;
+    const { dataSource } = this.state.configCharts;
 
-        const {data} = this.state.configCharts.dataSource;
-        const {items} = this.state.configCharts.dataSource.markers;
-        MeteoDataLoader.loadData().then(() => this.setState({meteoLoaded: true}));
-        YieldsDataLoader.loadData()
-            .then(() => this.setState({yieldsLoaded: true}))
-            .then(() => {
-                data.forEach(region => {
-                    region.value = YieldsDataLoader.avgOf(
-                        "all",
-                        "dt/ha",
-                        `PL-${region.shortLabel}`
-                    );
-                });
-                let i = 0;
-                for(var r in Regions){
-                    items[i] = {
-                        "id": r + "_rainfall",
-                        "shapeid": "we-anchor",
-                        "x": Regions[r].x ,
-                        "y": Regions[r].y - 10,
-                        "label":  r + "_rainfall",
-                        "value": "3",
-                        "tooltext":"",
-                        "labelpos": "bottom"
-                    };
-                    i++;
-                    items[i] = {
-                        "id": r + "_temp",
-                        "shapeid": "aws-anchor",
-                        "x": Regions[r].x,
-                        "y": Regions[r].y + 15,
-                        "label":  r + "_temp",
-                        "value": "3",
-                        "tooltext": "",//new MeteoDataLoader().avgOf("temp", 2003, r),
-                        "labelpos": "bottom"
-                    };
-                    i++;
-                }
-                this.setState({
-                    configCharts: {
-                        ...configCharts,
-                        dataSource: {...dataSource, data: data}
-                    }
-                });
-            });
+    const { data } = this.state.configCharts.dataSource;
+    const { items } = this.state.configCharts.dataSource.markers;
+    MeteoDataLoader.loadData()
+      .then(() => this.setState({ meteoLoaded: true }))
+      .then(() => {
+        console.log(new MeteoDataLoader().avgOf("temp", "all", "PL-DS", 1, 12));
+        console.log(new MeteoDataLoader().avgOf("temp", 2004, "PL-DS", 1, 12));
+      });
+    YieldsDataLoader.loadData()
+      .then(() => this.setState({ yieldsLoaded: true }))
+      .then(() => {
+        data.forEach(region => {
+          region.value = YieldsDataLoader.avgOf(
+            "all",
+            "dt/ha",
+            `PL-${region.shortLabel}`
+          );
+        });
+        let i = 0;
+        for (var r in Regions) {
+          items[i] = {
+            id: r + "_rainfall",
+            shapeid: "we-anchor",
+            x: Regions[r].x,
+            y: Regions[r].y - 10,
+            label: r + "_rainfall",
+            value: new MeteoDataLoader().avgOf("rainfall", "all", r, 1, 12),
+            tooltext: "",
+            labelpos: "bottom"
+          };
+          i++;
+          items[i] = {
+            id: r + "_temp",
+            shapeid: "aws-anchor",
+            x: Regions[r].x,
+            y: Regions[r].y + 15,
+            label: r + "_temp",
+            value: new MeteoDataLoader().avgOf("temp", "all", r, 1, 12),
+            tooltext: "",
+            labelpos: "bottom"
+          };
+          i++;
+        }
+        this.setState({
+          configCharts: {
+            ...configCharts,
+            dataSource: { ...dataSource, data: data }
+          }
+        });
+      });
+  }
+
+  toggleRegion = (event, args) => {
+    const { regions } = this.state;
+    const { configCharts } = this.state;
+    const { dataSource } = this.state.configCharts;
+    const { data } = this.state.configCharts.dataSource;
+    const newData = data.filter(elem => elem.shortLabel !== args.shortLabel);
+    let row = data.find(elem => elem.shortLabel === args.shortLabel);
+    if (regions.includes("PL-" + args.shortLabel)) {
+      delete row["color"];
+      this.setState({
+        configCharts: {
+          ...configCharts,
+          dataSource: { ...dataSource, data: [...newData, { ...row }] }
+        }
+      });
+    } else {
+      this.setState({
+        configCharts: {
+          ...configCharts,
+          dataSource: {
+            ...dataSource,
+            data: [...newData, { ...row, color: "black" }]
+          }
+        }
+      });
     }
 
-    toggleRegion = (event, args) => {
-        const {regions} = this.state;
-        const {configCharts} = this.state;
-        const {dataSource} = this.state.configCharts;
-        const {data} = this.state.configCharts.dataSource;
-        const newData = data.filter(elem => elem.shortLabel !== args.shortLabel);
-        let row = data.find(elem => elem.shortLabel === args.shortLabel);
-        if (regions.includes("PL-" + args.shortLabel)) {
-            delete row["color"];
-            this.setState({
-                configCharts: {
-                    ...configCharts,
-                    dataSource: {...dataSource, data: [...newData, {...row}]}
-                }
-            });
-        } else {
-            this.setState({
-                configCharts: {
-                    ...configCharts,
-                    dataSource: {
-                        ...dataSource,
-                        data: [...newData, {...row, color: "black"}]
-                    }
-                }
-            });
-        }
-
-        if (regions.includes("PL-" + args.shortLabel)) {
-            let newRegions = regions.filter(
-                region => region !== "PL-" + args.shortLabel
-            );
-            this.setState({regions: newRegions});
-        } else {
-            this.setState({
-                regions: [...this.state.regions, "PL-" + args.shortLabel]
-            });
-        }
-    };
-
-    render() {
-        const {meteoLoaded, yieldsLoaded, regions, configCharts} = this.state;
-        if (meteoLoaded && yieldsLoaded) {
-            return (
-                <div className="App">
-                    <Map chartConfigs={configCharts} toggleRegion={this.toggleRegion}/>
-                    {regions.length !== 0 && <ChartsGroup regions={regions}/>}
-                </div>
-            );
-        } else {
-            return <div>Loading data ...</div>;
-        }
+    if (regions.includes("PL-" + args.shortLabel)) {
+      let newRegions = regions.filter(
+        region => region !== "PL-" + args.shortLabel
+      );
+      this.setState({ regions: newRegions });
+    } else {
+      this.setState({
+        regions: [...this.state.regions, "PL-" + args.shortLabel]
+      });
     }
+  };
+
+  render() {
+    const { meteoLoaded, yieldsLoaded, regions, configCharts } = this.state;
+    if (meteoLoaded && yieldsLoaded) {
+      return (
+        <div className="App">
+          <Map chartConfigs={configCharts} toggleRegion={this.toggleRegion} />
+          {regions.length !== 0 && <ChartsGroup regions={regions} />}
+        </div>
+      );
+    } else {
+      return <div>Loading data ...</div>;
+    }
+  }
 }
 
 export default App;
--- src/utils/MeteoDataLoader.js
+++ src/utils/MeteoDataLoader.js
@@ -43,45 +43,101 @@ export default class MeteoDataLoader {
       );
     }
 
-    return this.data
+    return this.data;
   }
 
   avgOf(type = "rainfall", year = 2003, region = "PL-DS", from = 8, to = 9) {
-
     const average = () => {
       switch (type.toUpperCase()) {
         case "TEMP":
           this.pickedData = MeteoDataLoader.data[`climatic${year}`].filter(
             row =>
-              row.Region === region.toUpperCase() && row.Miesiąc >= from && row.Miesiąc <= to
+              row.Region === region.toUpperCase() &&
+              row.Miesiąc >= from &&
+              row.Miesiąc <= to
           );
-          result = this.pickedData.reduce((a, b) => a + b.Średnia_miesięczna_temperatura, 0) / this.pickedData.length
+          result =
+            this.pickedData.reduce(
+              (a, b) => a + b.Średnia_miesięczna_temperatura,
+              0
+            ) / this.pickedData.length;
           break;
         case "HUMIDITY":
           this.pickedData = MeteoDataLoader.data[`climatic${year}`].filter(
             row =>
-              row.Region === region.toUpperCase() && row.Miesiąc >= from && row.Miesiąc <= to
+              row.Region === region.toUpperCase() &&
+              row.Miesiąc >= from &&
+              row.Miesiąc <= to
           );
-          result = this.pickedData.reduce((a, b) => a + b.Średnia_miesięczna_wilgotność_względna, 0) / this.pickedData.length
+          result =
+            this.pickedData.reduce(
+              (a, b) => a + b.Średnia_miesięczna_wilgotność_względna,
+              0
+            ) / this.pickedData.length;
           break;
         default:
           this.pickedData = MeteoDataLoader.data[`meteo${year}`].filter(
             row =>
-              row.Region === region.toUpperCase() && row.Miesiąc >= from && row.Miesiąc <= to
+              row.Region === region.toUpperCase() &&
+              row.Miesiąc >= from &&
+              row.Miesiąc <= to
           );
-          result = this.pickedData.reduce((a, b) => a + b.Miesięczna_suma_opadów, 0) / this.pickedData.length
+          result =
+            this.pickedData.reduce((a, b) => a + b.Miesięczna_suma_opadów, 0) /
+            this.pickedData.length;
           break;
       }
-    }
+    };
+
+    const averageAll = () => {
+      switch (type.toUpperCase()) {
+        case "TEMP":
+          for (let i = 2003; i < 2017; i++) {
+            this.pickedData = MeteoDataLoader.data[`climatic${i}`].filter(
+              row =>
+                row.Region === region.toUpperCase() &&
+                row.Miesiąc >= from &&
+                row.Miesiąc <= to
+            );
+            result +=
+              this.pickedData.reduce(
+                (a, b) => a + b.Średnia_miesięczna_temperatura,
+                0
+              ) / this.pickedData.length;
+          }
+          result = result / 14;
+          break;
+        default:
+        for (let i = 2003; i < 2017; i++) {
+          this.pickedData = MeteoDataLoader.data[`meteo${i}`].filter(
+            row =>
+              row.Region === region.toUpperCase() &&
+              row.Miesiąc >= from &&
+              row.Miesiąc <= to
+          );
+          result +=
+            this.pickedData.reduce((a, b) => a + b.Miesięczna_suma_opadów, 0) /
+            this.pickedData.length;
+          }
+          result = result / 14;
+          break;
+      }
+    };
 
     let result = 0;
 
     if (Object.keys(MeteoDataLoader.data).length === 0) {
       MeteoDataLoader.loadData().then(() => {
-        average()
-      })
+        if (typeof year === "number") {
+          average();
+        }
+        averageAll();
+      });
     } else {
-      average()
+      if (typeof year === "number") {
+        average();
+      }
+      averageAll();
     }
 
     return result;
