9d84c25219855bd472c0122fc64f01adca5b36f8
Damian Szalbierz <szalbierz.d.k@gmail.com>
2019-01-13 13:22:32 +0100

    Refactor yielddataloader and load data to map

--- src/components/App.js
+++ src/components/App.js
@@ -1,118 +1,237 @@
-import React, { Component } from 'react';
-import ChartsGroup from './ChartsGroup';
-import Map from './Map';
-import MeteoDataLoader from '../utils/MeteoDataLoader';
-import './App.css';
+import React, { Component } from "react";
+import ChartsGroup from "./ChartsGroup";
+import Map from "./Map";
+import MeteoDataLoader from "../utils/MeteoDataLoader";
+import "./App.css";
 import YieldsDataLoader from "../utils/YieldsDataLoader";
 
 class App extends Component {
-    state = {
-        meteoLoaded: false,
-        yieldsLoaded: false,
-        configCharts: {
-            type: 'maps/poland',
-            height: '30%',
-            width: '100%',
-            dataFormat: 'json',
-            dataSource: {
-            // Map Configuration
-                "chart": {
-                        "caption": "Average Yield in Poland",
-                        "subcaption": " 2003-2016",
-                        "numbersuffix": "",
-                        "includevalueinlabels": "1",
-                        "labelsepchar": ": ",
-                        "entityFillHoverColor": "#FFF9C4",
-                        "theme": "fusion"
-                },
-                // Aesthetics; ranges synced with the slider
-                "colorrange": {
-                    "minvalue": "0",
-                    "code": "#FFE0B2",
-                    "gradient": "1",
-                    "color": [{
-                        "minvalue": "0.5",
-                        "maxvalue": "300",
-                        "color": "#FFD74D"
-                    }, {
-                        "minvalue": "300",
-                        "maxvalue": "700",
-                        "color": "#FB8C00"
-                    }, {
-                        "minvalue": "700",
-                        "maxvalue": "1000",
-                        "color": "#E65100"
-                    }]
-                },
-                // Source data as JSON --> id represents regions of Poland.
-                "data": [
-                    { "id": "74", "value": Math.random()*1000, "label": "Łódzkie", "shortLabel": "LD" },
-                    { "id": "72", "value": YieldsDataLoader.avgOf("all", "dt/ha", "PL-DS"), "label": "Dolnośląskie", "shortLabel": "DS", color: 'black' },
-                    { "id": "73", "value": Math.random()*1000, "label": "Kujawsko-Pomorskie", "shortLabel": "KP" },
-                    { "id": "75", "value": Math.random()*1000, "label": "Lubelskie", "shortLabel": "LU" },
-                    { "id": "76", "value": Math.random()*1000, "label": "Lubuskie", "shortLabel": "LB" },
-                    { "id": "78", "value": Math.random()*1000, "label": "Mazowieckie", "shortLabel": "MZ" },
-                    { "id": "77", "value": Math.random()*1000, "label": "Małopolskie", "shortLabel": "MA" },
-                    { "id": "79", "value": Math.random()*1000, "label": "Opolskie", "shortLabel": "OP" },
-                    { "id": "80", "value": Math.random()*1000, "label": "Podkarpackie", "shortLabel": "PK" },
-                    { "id": "81", "value": Math.random()*1000, "label": "Podlaskie", "shortLabel": "PD" },
-                    { "id": "82", "value": Math.random()*1000, "label": "Pomorskie", "shortLabel": "PM" },
-                    { "id": "85", "value": Math.random()*1000, "label": "Warmińsko-Mazurskie", "shortLabel": "WN" },
-                    { "id": "86", "value": Math.random()*1000, "label": "Wielkopolskie", "shortLabel": "WP", color: 'black' },
-                    { "id": "87", "value": Math.random()*1000, "label": "Zachodniopomorskie", "shortLabel": "ZP" },
-                    { "id": "83", "value": Math.random()*1000, "label": "Śląskie", "shortLabel": "SL" },
-                    { "id": "84", "value": Math.random()*1000, "label": "Świętokrzyskie", "shortLabel": "SK" },
-                ]
-            }
+  state = {
+    meteoLoaded: false,
+    yieldsLoaded: false,
+    regionsDataLoaded: false,
+    configCharts: {
+      type: "maps/poland",
+      height: "30%",
+      width: "100%",
+      dataFormat: "json",
+      dataSource: {
+        // Map Configuration
+        chart: {
+          caption: "Average Yield in Poland",
+          subcaption: " 2003-2016",
+          numbersuffix: "",
+          includevalueinlabels: "1",
+          labelsepchar: ": ",
+          entityFillHoverColor: "#FFF9C4",
+          theme: "fusion"
         },
-        regions: ['PL-WP', 'PL-DS'],
-    }
+        // Aesthetics; ranges synced with the slider
+        colorrange: {
+          minvalue: "0",
+          code: "#FFE0B2",
+          gradient: "1",
+          color: [
+            {
+              minvalue: "0",
+              maxvalue: "10",
+              color: "#FFD74D"
+            },
+            {
+              minvalue: "10",
+              maxvalue: "13",
+              color: "#FB8C00"
+            },
+            {
+              minvalue: "13",
+              maxvalue: "19",
+              color: "#E65100"
+            }
+          ]
+        },
+        // Source data as JSON --> id represents regions of Poland.
+        data: [
+          {
+            id: "74",
+            value: Math.random() * 1000,
+            label: "Łódzkie",
+            shortLabel: "LD"
+          },
+          {
+            id: "72",
+            value: YieldsDataLoader.avgOf("all", "dt/ha", "PL-DS"),
+            label: "Dolnośląskie",
+            shortLabel: "DS",
+            color: "black"
+          },
+          {
+            id: "73",
+            value: Math.random() * 1000,
+            label: "Kujawsko-Pomorskie",
+            shortLabel: "KP"
+          },
+          {
+            id: "75",
+            value: Math.random() * 1000,
+            label: "Lubelskie",
+            shortLabel: "LU"
+          },
+          {
+            id: "76",
+            value: Math.random() * 1000,
+            label: "Lubuskie",
+            shortLabel: "LB"
+          },
+          {
+            id: "78",
+            value: Math.random() * 1000,
+            label: "Mazowieckie",
+            shortLabel: "MZ"
+          },
+          {
+            id: "77",
+            value: Math.random() * 1000,
+            label: "Małopolskie",
+            shortLabel: "MA"
+          },
+          {
+            id: "79",
+            value: Math.random() * 1000,
+            label: "Opolskie",
+            shortLabel: "OP"
+          },
+          {
+            id: "80",
+            value: Math.random() * 1000,
+            label: "Podkarpackie",
+            shortLabel: "PK"
+          },
+          {
+            id: "81",
+            value: Math.random() * 1000,
+            label: "Podlaskie",
+            shortLabel: "PD"
+          },
+          {
+            id: "82",
+            value: Math.random() * 1000,
+            label: "Pomorskie",
+            shortLabel: "PM"
+          },
+          {
+            id: "85",
+            value: Math.random() * 1000,
+            label: "Warmińsko-Mazurskie",
+            shortLabel: "WN"
+          },
+          {
+            id: "86",
+            value: Math.random() * 1000,
+            label: "Wielkopolskie",
+            shortLabel: "WP",
+            color: "black"
+          },
+          {
+            id: "87",
+            value: Math.random() * 1000,
+            label: "Zachodniopomorskie",
+            shortLabel: "ZP"
+          },
+          {
+            id: "83",
+            value: Math.random() * 1000,
+            label: "Śląskie",
+            shortLabel: "SL"
+          },
+          {
+            id: "84",
+            value: Math.random() * 1000,
+            label: "Świętokrzyskie",
+            shortLabel: "SK"
+          }
+        ]
+      }
+    },
+    regions: ["PL-WP", "PL-DS"]
+  };
 
-    componentDidMount() {
-        MeteoDataLoader.loadData().then(() => this.setState({ meteoLoaded: true }))
-        YieldsDataLoader.loadData().then(() => { this.setState({ yieldsLoaded: true }) })
-    }
+  componentDidMount() {
+    const { configCharts } = this.state;
+    const { dataSource } = this.state.configCharts;
 
-    toggleRegion = (event, args) => {
-        const { regions } = this.state;
-        const { configCharts } = this.state;
-        const { dataSource } = this.state.configCharts;
-        const { data } = this.state.configCharts.dataSource;
-        const newData = data.filter(elem => elem.shortLabel !== args.shortLabel)
-        let row = data.find(elem => elem.shortLabel === args.shortLabel)
-        if (regions.includes("PL-"+args.shortLabel)) {
-            delete row['color']
-            this.setState({ configCharts: { ...configCharts, dataSource: { ...dataSource, data: [...newData, { ...row } ] }  } })
-        } else {
-            this.setState({ configCharts: { ...configCharts, dataSource: { ...dataSource, data: [...newData, { ...row, color: "black" } ] }  } })
+    const { data } = this.state.configCharts.dataSource;
+    MeteoDataLoader.loadData().then(() => this.setState({ meteoLoaded: true }));
+    YieldsDataLoader.loadData()
+      .then(() => this.setState({ yieldsLoaded: true }))
+      .then(() => {
+        data.forEach(region => {
+          region.value = YieldsDataLoader.avgOf(
+            "all",
+            "dt/ha",
+            `PL-${region.shortLabel}`
+          );
+        });
+        this.setState({
+          configCharts: {
+            ...configCharts,
+            dataSource: { ...dataSource, data: data }
+          }
+        });
+      });
+  }
+
+  toggleRegion = (event, args) => {
+    const { regions } = this.state;
+    const { configCharts } = this.state;
+    const { dataSource } = this.state.configCharts;
+    const { data } = this.state.configCharts.dataSource;
+    const newData = data.filter(elem => elem.shortLabel !== args.shortLabel);
+    let row = data.find(elem => elem.shortLabel === args.shortLabel);
+    if (regions.includes("PL-" + args.shortLabel)) {
+      delete row["color"];
+      this.setState({
+        configCharts: {
+          ...configCharts,
+          dataSource: { ...dataSource, data: [...newData, { ...row }] }
         }
-
-        if (regions.includes("PL-"+args.shortLabel)) {
-            let newRegions = regions.filter(region => region !== "PL-"+args.shortLabel)
-            this.setState({regions: newRegions})
-        } else {
-            this.setState({regions: [...this.state.regions, "PL-"+args.shortLabel]})
+      });
+    } else {
+      this.setState({
+        configCharts: {
+          ...configCharts,
+          dataSource: {
+            ...dataSource,
+            data: [...newData, { ...row, color: "black" }]
+          }
         }
+      });
     }
 
-    updateRegionsData = () => {
-        let { configCharts } = this.state;
+    if (regions.includes("PL-" + args.shortLabel)) {
+      let newRegions = regions.filter(
+        region => region !== "PL-" + args.shortLabel
+      );
+      this.setState({ regions: newRegions });
+    } else {
+      this.setState({
+        regions: [...this.state.regions, "PL-" + args.shortLabel]
+      });
     }
+  };
 
-    render() {
-        const { meteoLoaded, yieldsLoaded, regions, configCharts } = this.state;
-        if(meteoLoaded && yieldsLoaded) {
-            console.log(YieldsDataLoader.avgOf("all", "dt/ha", "PL-DS"))
-            return (
-                <div className="App">
-                    <Map chartConfigs={configCharts} toggleRegion={this.toggleRegion} />
-                    {regions.length !== 0 && <ChartsGroup regions={regions}/>}
-                </div>
-            );
-        }  else {
-            return (<div>Loading data ...</div>)
-        }
+  render() {
+    const { meteoLoaded, yieldsLoaded, regions, configCharts } = this.state;
+    if (meteoLoaded && yieldsLoaded) {
+      return (
+        <div className="App">
+          <Map chartConfigs={configCharts} toggleRegion={this.toggleRegion} />
+          {regions.length !== 0 && <ChartsGroup regions={regions} />}
+        </div>
+      );
+    } else {
+      return <div>Loading data ...</div>;
     }
+  }
 }
 
 export default App;
--- src/utils/YieldsDataLoader.js
+++ src/utils/YieldsDataLoader.js
@@ -1,85 +1,116 @@
-import * as d3  from 'd3';
+import * as d3 from "d3";
 
 const regionsMap = {
-    "PL-DS": {rowNumber: 1, name: "dolnośląskie"},
-    "PL-KP": {rowNumber: 2, name: "kujawsko-pomorskie"},
-    "PL-LU": {rowNumber: 3, name: "lubelskie"},
-    "PL-LB": {rowNumber: 4, name: "lubuskie"},
-    "PL-LD": {rowNumber: 5, name: "łódzkie"},
-    "PL-MA": {rowNumber: 6, name: "małopolskie"},
-    "PL-MZ": {rowNumber: 7, name: "mazowieckie"},
-    "PL-OP": {rowNumber: 5, name: "opolskie"},
-    "PL-PK": {rowNumber: 9, name: "podkarpackie"},
-    "PL-PD": {rowNumber: 10, name: "podlaskie"},
-    "PL-PM": {rowNumber: 11, name: "pomorskie"},
-    "PL-SL": {rowNumber: 12, name: "śląskie"},
-    "PL-SK": {rowNumber: 13, name: "świętokrzyskie"},
-    "PL-WN": {rowNumber: 14, name: "warmińsko-mazurskie"},
-    "PL-WP": {rowNumber: 15, name: "wielkopolskie"},
-    "PL-ZP": {rowNumber: 16, name: "zachodniopomorskie"},
+  "PL-DS": { rowNumber: 1, name: "dolnośląskie" },
+  "PL-KP": { rowNumber: 2, name: "kujawsko-pomorskie" },
+  "PL-LU": { rowNumber: 3, name: "lubelskie" },
+  "PL-LB": { rowNumber: 4, name: "lubuskie" },
+  "PL-LD": { rowNumber: 5, name: "łódzkie" },
+  "PL-MA": { rowNumber: 6, name: "małopolskie" },
+  "PL-MZ": { rowNumber: 7, name: "mazowieckie" },
+  "PL-OP": { rowNumber: 5, name: "opolskie" },
+  "PL-PK": { rowNumber: 9, name: "podkarpackie" },
+  "PL-PD": { rowNumber: 10, name: "podlaskie" },
+  "PL-PM": { rowNumber: 11, name: "pomorskie" },
+  "PL-SL": { rowNumber: 12, name: "śląskie" },
+  "PL-SK": { rowNumber: 13, name: "świętokrzyskie" },
+  "PL-WN": { rowNumber: 14, name: "warmińsko-mazurskie" },
+  "PL-WP": { rowNumber: 15, name: "wielkopolskie" },
+  "PL-ZP": { rowNumber: 16, name: "zachodniopomorskie" }
 };
 
 class YieldsDataLoader {
-    static data = [];
-    static yieldsTypes = ['gryka', 'jeczmien', 'kukurydza_na_ziarno', 'owies','proso', 'pszenica', 'zyto', 'pszenzyto', 'rzepak_i_rzepik', 'ziemniaki', 'buraki_cukrowe'];
+  static data = [];
+  static yieldsTypes = [
+    "gryka",
+    "jeczmien",
+    "kukurydza_na_ziarno",
+    "owies",
+    "proso",
+    "pszenica",
+    "zyto",
+    "pszenzyto",
+    "rzepak_i_rzepik",
+    "ziemniaki",
+    "buraki_cukrowe"
+  ];
 
-    static single(year, yieldName, columnName, region) {
-        let regionRow = regionsMap[region].rowNumber;
-        if(typeof this.data[yieldName] !== 'undefined' && typeof this.data[yieldName][year] !== 'undefined'){
-            return this.data[yieldName][year][regionRow][columnName].replace(/\s/g, '').replace(/,/g, '.');
-        }
-        return null;
+  static single(year, yieldName, columnName, region) {
+    let regionRow = regionsMap[region].rowNumber;
+    if (
+      typeof this.data[yieldName] !== "undefined" &&
+      typeof this.data[yieldName][year] !== "undefined"
+    ) {
+      return this.data[yieldName][year][regionRow][columnName]
+        .replace(/\s/g, "")
+        .replace(/,/g, ".");
     }
+    return null;
+  }
 
-    static async loadSingleFile(year, yieldName){
-        let parsedData = [];
-        await d3.csv('data/yields/' + year + "_" + yieldName + ".csv", data => parsedData.push(data));
-        if(typeof this.data[yieldName] === 'undefined'){
-            this.data[yieldName] = [];
-        }
-        this.data[yieldName][year] = parsedData;
-        return parsedData;
+  static async loadSingleFile(year, yieldName) {
+    let parsedData = [];
+    await d3.csv("data/yields/" + year + "_" + yieldName + ".csv", data =>
+      parsedData.push(data)
+    );
+    if (typeof this.data[yieldName] === "undefined") {
+      this.data[yieldName] = [];
     }
+    this.data[yieldName][year] = parsedData;
+    return parsedData;
+  }
 
-    /**
-     *
-     * @param yieldName (yieldName='all' return avg for all yields)
-     * @param columnName
-     * @param region
-     * @param yearStart
-     * @param yearStop
-     * @returns {number}
-     */
-    static avgOf(yieldName, columnName, region, yearStart=2003, yearStop=2016) {
-        let sum = 0;
-        let year = yearStart;
-        if(yieldName === 'all'){
-            let counter = 0;
-            this.yieldsTypes.forEach(yieldName => {
-                for (year; year <= yearStop; year++) {
-                    counter++;
-                    sum += parseFloat(this.single(year, yieldName, columnName, region));
-                }
-            });
-            return sum / counter;
+  /**
+   *
+   * @param yieldName (yieldName='all' return avg for all yields)
+   * @param columnName
+   * @param region
+   * @param yearStart
+   * @param yearStop
+   * @returns {number}
+   */
+  static avgOf(
+    yieldName,
+    columnName,
+    region,
+    yearStart = 2003,
+    yearStop = 2016
+  ) {
+    let sum = 0;
+    let year = yearStart;
+    if (yieldName === "all") {
+      let counter = 0;
+      this.yieldsTypes.forEach(yieldName => {
+        for (year; year <= yearStop; year++) {
+          counter++;
+          sum += parseFloat(this.single(year, yieldName, columnName, region));
         }
-        else {
-            for (year; year <= yearStop; year++) {
-                sum += parseFloat(this.single(year, yieldName, columnName, region));
-            }
-        }
-        return sum / (yearStop - yearStart + 1);
+      });
+      return sum / counter;
+    } else {
+      for (year; year <= yearStop; year++) {
+        sum += parseFloat(this.single(year, yieldName, columnName, region));
+      }
     }
+    return sum / (yearStop - yearStart + 1);
+  }
 
-    static async loadData(){
-        this.yieldsTypes.forEach(y => {
-            let year = 2003;
-            for(year; year <= 2016; year++){
-                this.loadSingleFile(year, y);
-            }
-        })
-        return this.data;
+  static async loadData() {
+    await this.asyncForEach(this.yieldsTypes, async y => {
+      let year = 2003;
+      for (year; year <= 2016; year++) {
+        await this.loadSingleFile(year, y);
+      }
+    });
+    return this.data;
+  }
+
+//  Need to create async forEach
+  static async asyncForEach(array, callback) {
+    for (let index = 0; index < array.length; index++) {
+      await callback(array[index], index, array);
     }
+  }
 }
 
-export default YieldsDataLoader
+export default YieldsDataLoader;
